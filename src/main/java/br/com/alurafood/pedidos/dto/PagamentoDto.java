e9dd6cb154566f36eb24ad99e7a7b7d934d78941
Usuario <Usuario@RICARDO-NITRO>
2023-06-15 17:14:31 -0300

    ajuste message

---
+++ src/main/java/br/com/alurafood/pedidos/dto/PagamentoDto.java
@@ -0,0 +1,25 @@
+package br.com.alurafood.pedidos.dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import br.com.alurafood.pedidos.model.ItemDoPedido;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PagamentoDto {
+
+        private Long id;
+    private BigDecimal valor;
+    private String nome;
+    private String numero;
+    private String expiracao;
+    private String codigo;
+    private StatusPagamento status;
+    private Long formaDePagamentoId;
+    private Long pedidoId;
+    private List<ItemDoPedido> itens;
+    
+}
